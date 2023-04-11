7e4f5fb6fd13559c97ed5bc2f09250648e086f5b
bleezane <men.nael@yahoo.com>
2023-04-11 16:20:49 +0200

    initialisation

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Accueil</router-link> |
+    <router-link to="/magasin">magasin</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: right;
+  color: #50342c;
+-ms-text-align-last: right
+}
+template
+#nav {
+  padding: 25px;
+}
+script
+#nav a {
+  font-weight: bold;
+  color: #2c5037;
+}
+
+#nav a.router-link-exact-active {
+  color: #4297b9;
+}
+</style>
---
+++ src/views/Magasin.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="magasin">
+    <h1>This is an magasin page</h1>
+
+<img src="../assets/magasin_noel.jpg" width="300" />
+
+
+  </div>
+</template>
